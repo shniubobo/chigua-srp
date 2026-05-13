77f74e7f2583fb27b3bd2828b77718f3a2848e58
shniubobo <me@shniubobo.com>
2026-05-13 17:51:54 +0800

    Add `handleAuth` to `oauth-proxy`

--- oauth-proxy/src/index.test.ts
+++ oauth-proxy/src/index.test.ts
@@ -1,16 +1,27 @@
 import { describe, expect, it as itBase } from "vitest";
 
 import { exports } from "cloudflare:workers";
-import { HTTP_STATUS, OpenIdConfig, PATH_CONFIG } from "./index";
+import {
+  HTTP_STATUS,
+  OpenIdConfig,
+  PATH_AUTH,
+  PATH_CALLBACK,
+  PATH_CONFIG,
+} from "./index";
 
 const WORKER_BASE = new URL("https://worker.com");
 const UPSTREAM_BASE = new URL("https://upstream.com");
+const DOWNSTREAM_BASE = new URL("https://downstream.com");
 
 async function makeRequest(
   pathname: string,
   method: string = "GET",
+  base: URL = WORKER_BASE,
 ): Promise<Response> {
-  return exports.default.fetch(new URL(pathname, WORKER_BASE), { method });
+  return exports.default.fetch(new URL(pathname, base), {
+    method,
+    redirect: "manual",
+  });
 }
 
 const it = itBase.extend("faultyUpstream", async ({}, { onCleanup }) => {
@@ -52,20 +63,79 @@ describe("openid config", () => {
     },
   );
 
-  it("should fallback to 404 on non-GET requests", async () => {
-    const resp = await makeRequest(PATH_CONFIG, "POST");
-    expect(resp.status).toBe(HTTP_STATUS.NOT_FOUND);
-  });
-
   it("should return 502 on faulty upstream", async ({ faultyUpstream: _ }) => {
     const resp = await makeRequest(PATH_CONFIG);
     expect(resp.status).toBe(HTTP_STATUS.BAD_GATEWAY);
   });
 });
 
+describe("auth", () => {
+  const pathnameShouldPass = encodeURI(
+    `${PATH_AUTH}?redirect_uri=${DOWNSTREAM_BASE.toString()}&foo=bar`,
+  );
+
+  it("should rewrite redirect_uri, retain other params, set cookies, and redirect", async () => {
+    const resp = await makeRequest(pathnameShouldPass);
+
+    expect(resp.status).toBe(HTTP_STATUS.TEMPORARY_REDIRECT);
+    expect(Object.fromEntries(resp.headers.entries())).toMatchInlineSnapshot(`{
+  "location": "https://login.eveonline.com/v2/oauth/authorize?redirect_uri=https%3A%2F%2Fworker.com%2Fcallback&foo=bar",
+  "set-cookie": "redirect-uri=https%3A%2F%2Fdownstream.com%2F; Path=/callback; HttpOnly; SameSite=Lax",
+}`);
+  });
+
+  it.for([
+    "foo=bar",
+    `redirect_uri=${encodeURIComponent("https//upstream.com")}`,
+  ])("should return 400 on missing or invalid redirect_uri", async (params) => {
+    const resp = await makeRequest(`${PATH_AUTH}?${params}`);
+    expect(resp.status).toBe(HTTP_STATUS.BAD_REQUEST);
+  });
+
+  describe("location header", () => {
+    it("should have correct protocol, hostname and no port", async () => {
+      const resp = await makeRequest(
+        pathnameShouldPass,
+        "GET",
+        new URL("http://worker.com:5137"),
+      );
+      expect(resp.status).toBe(HTTP_STATUS.TEMPORARY_REDIRECT);
+      expect(resp.headers.get("location")).toMatch(
+        /^https:\/\/login.eveonline.com\//,
+      );
+    });
+  });
+
+  describe("rewritten redirect_uri", () => {
+    it.for(["http://127.0.0.1:5137", "https://preview.worker.com"])(
+      "should be based on current url",
+      async (base) => {
+        const resp = await makeRequest(
+          pathnameShouldPass,
+          "GET",
+          new URL(base),
+        );
+        expect(resp.status).toBe(HTTP_STATUS.TEMPORARY_REDIRECT);
+
+        const location = new URL(resp.headers.get("location") ?? "");
+        const redirectUri = location.searchParams.get("redirect_uri");
+        expect(redirectUri).toBe(`${base}${PATH_CALLBACK}`);
+      },
+    );
+  });
+});
+
 describe("fallback", () => {
   it.for(["/", "/foo"])("should return 404", async (pathname) => {
     const resp = await makeRequest(pathname);
+    expect(resp.status).toBe(HTTP_STATUS.NOT_FOUND);
+  });
+
+  it.for([
+    [PATH_CONFIG, "POST"],
+    [PATH_AUTH, "POST"],
+  ])("should fallback to 404 on wrong methods", async ([pathname, method]) => {
+    const resp = await makeRequest(pathname, method);
     expect(resp.status).toBe(HTTP_STATUS.NOT_FOUND);
   });
 });
--- oauth-proxy/src/index.ts
+++ oauth-proxy/src/index.ts
@@ -3,6 +3,9 @@ import * as z from "zod";
 export const URL_BASE = new URL("https://login.eveonline.com/");
 export const PATH_CONFIG = "/.well-known/openid-configuration";
 export const URL_CONFIG = new URL(PATH_CONFIG, URL_BASE);
+export const PATH_AUTH = "/v2/oauth/authorize";
+export const URL_AUTH = new URL(PATH_AUTH, URL_BASE);
+export const PATH_CALLBACK = "/callback";
 
 export const OpenIdConfig = z.looseObject({
   authorization_endpoint: z.url({ protocol: /^https?$/ }),
@@ -11,6 +14,8 @@ export const OpenIdConfig = z.looseObject({
 
 export const enum HTTP_STATUS {
   OK = 200,
+  TEMPORARY_REDIRECT = 307,
+  BAD_REQUEST = 400,
   NOT_FOUND = 404,
   INTERNAL_SERVER_ERROR = 500,
   BAD_GATEWAY = 502,
@@ -24,6 +29,8 @@ export default {
     switch (`${request.method}:${pathname}`) {
       case `GET:${PATH_CONFIG}`:
         return handleOpenIdConfig(request);
+      case `GET:${PATH_AUTH}`:
+        return handleAuth(request);
       default:
         return err(HTTP_STATUS.NOT_FOUND);
     }
@@ -71,6 +78,39 @@ async function handleOpenIdConfig(request: Request): Promise<Response> {
   configParsed.token_endpoint = tokenEndpoint.toString();
 
   return Response.json(configParsed);
+}
+
+function handleAuth(request: Request): Response {
+  const currentUrl = new URL(request.url);
+  const originalRedirectUri = currentUrl.searchParams.get("redirect_uri");
+  if (!originalRedirectUri || !URL.canParse(originalRedirectUri))
+    return err(HTTP_STATUS.BAD_REQUEST);
+
+  const newRedirectUri = new URL(currentUrl);
+  newRedirectUri.searchParams.set(
+    "redirect_uri",
+    new URL(PATH_CALLBACK, currentUrl).toString(),
+  );
+  newRedirectUri.protocol = URL_AUTH.protocol;
+  newRedirectUri.host = URL_AUTH.host;
+  // This has to be set seperately, as `URL_AUTH` lacks a port.
+  newRedirectUri.port = URL_AUTH.port;
+  newRedirectUri.pathname = URL_AUTH.pathname;
+  const cookieAttrs = [
+    `redirect-uri=${encodeURIComponent(originalRedirectUri)}`,
+    `Path=${PATH_CALLBACK}`,
+    "HttpOnly",
+    "SameSite=Lax",
+  ];
+  const headers = new Headers({
+    Location: newRedirectUri.toString(),
+    "Set-Cookie": cookieAttrs.join("; "),
+  });
+
+  return new Response(null, {
+    status: HTTP_STATUS.TEMPORARY_REDIRECT,
+    headers,
+  });
 }
 
 function isOk(status: number): boolean {
